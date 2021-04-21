e46e1976fb7a71b8310797f2639d965c204e9989
Fangxiu.Chen <fangxiu.chen@geely.com>
2021-04-21 16:55:15 +0800

    功能优化

--- src/main/java/com/rookie/opcua/utils/RedisUtils.java
+++ src/main/java/com/rookie/opcua/utils/RedisUtils.java
@@ -1,6 +1,5 @@
 package com.rookie.opcua.utils;
 
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.*;
 import org.springframework.stereotype.Service;
